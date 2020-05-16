c300e390b24fbad588d5a856705b49c23d9ef2d9
Gzoref <gzoref@gmail.com>
2020-05-16 14:43:52 -0400

    [Fix] JQuery for checkmarks

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,18 +1,16 @@
-$(document).ready(() => {
+window.onload = function () {
   const amenitiesCheck = {};
-
-  console.log(amenitiesCheck);
-
-  $('input:checkbox').on('change', () => {
+  $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
-      amenitiesCheck[$(this).data('id')] = $(this.data('name'));
-      console.log(amenitiesCheck);
+      amenitiesCheck[$(this).data('id')] = $(this).data('name');
     } else {
       delete amenitiesCheck[$(this).data('id')];
     }
-    $('div.amenites h4').text(Object.values(amenitiesCheck).join(', '));
-    if (Object.keys(amenitiesCheck).length === 0) {
-      $('div.amenites h4').text('\u00A0');
+    const amenitiesList = Object.values(amenitiesCheck);
+    if (amenitiesList.length > 0) {
+      $('div.amenities > h4').text(Object.values(amenitiesList).join(', '));
+    } else {
+      $('div.amenities > h4').html('&nbsp;');
     }
   });
-});
+};
