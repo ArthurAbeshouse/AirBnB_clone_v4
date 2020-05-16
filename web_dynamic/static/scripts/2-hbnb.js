eae7ffca89ee625eb0ef33b666addc0b0c0371f1
Gzoref <gzoref@gmail.com>
2020-05-16 15:23:33 -0400

    [Fix] typo in addClass

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -20,7 +20,7 @@ $.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
     if (data.status === 'OK') {
       $('div#api_status').addClass('available');
     } else {
-      $('div#api_statis').removeCass('available');
+      $('div#api_statis').removeClass('available');
     }
   }
 });
